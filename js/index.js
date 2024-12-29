a34a403a887961fd2e26c1c59ec77c828f3f3885
Jen Morrell <morrell616@hotmail.com>
2024-12-29 09:53:37 -0500

    added answers to answer.txt

--- js/index.js
+++ js/index.js
@@ -1,4 +1,48 @@
+
+/*
 // Prevent us from attempting to use variables
 // that are not declared
-"use strict"
+"use strict";
+
+console.log(1 + 1);
+console.log(1-1);
+console.log(1 + 1);         // Output: 2
+console.log(1 - 1);          // Output: 0
+console.log(1 + "1");  
+console.log(1 + "2");
+console.log(1 + "3");   // Output: 11
+                             // What? Why? Search for the answer
+
+console.log(3 - 4 * 5)       // Output: -17
+console.log((3 - 4) * 5);    // Output: -5
+                             // Remember order of precendence
+
+console.log(6 / 5);          // Output: 1.2
+console.log(6 % 5);          // Output: 1
+                             // Modulus return the remainder. This operator is extremely useful.
+
+console.log(1 == 1);         // Output: true
+console.log(1 == "1");       // Output: true
+console.log(1 === "1");      // Output: false
+
+console.log(1 || 2);         // Output: 1
+console.log(2 || 1);         // Output: 2
+console.log(1 && 2);         // Output: false
+console.log(1 && 1);         // Output: true
+
+const x = 1 + 2 * 3;
+const y = 2 * 3 + 1;
+
+const obj = {};
+
+obj.x = 3;
+console.log(obj.x); // Prints 3.
+console.log(obj); // Prints { x: 3 }.
+
+const key = "y";
+obj[key] = 5;
+console.log(obj[key]); // Prints 5.
+console.log(obj); // Prints { x: 3, y: 5 }.
+*/
+
 
